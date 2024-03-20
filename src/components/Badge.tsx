2ca005b488d5c91013893b1eea44fcf8453d6553
Ben Field <fieldben97@gmail.com>
2024-03-20 15:11:19 +1100

    multiline actual

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Airstack</span>
+        <span> Airstack test</span>
       </a>
     </span>
   );
